e8f36cce42e3d1d51ea4c897d22003004f93f8d6
Anton Astashov <anton.astashov@gmail.com>
2014-02-13 14:51:02 -0800

    Make sure any exceptions won't break the app

--- lib/rollbar.dart
+++ lib/rollbar.dart
@@ -12,9 +12,15 @@ install(String accessToken, body(), {Map<String, Object> customPayload(), Logger
   runZoned(() {
     result = body();
   }, onError: (error, stackTrace) {
-    var rollbar = new Rollbar(accessToken, error, stackTrace, customPayload: customPayload, logger: logger);
-    rollbar.send();
-    // TODO: Find way to rethrow an error with preserved stack trace
+    try {
+      var rollbar = new Rollbar(accessToken, error, stackTrace, customPayload: customPayload, logger: logger);
+      rollbar.send();
+      // TODO: Find way to rethrow an error with preserved stack trace
+    } catch (e) {
+      if (logger != null) {
+        logger.finer("Exception happened while trying to send error to Rollbar: $e");
+      }
+    }
     print("Uncaught Error: $error");
     print("Stack Trace:");
     print(stackTrace);
